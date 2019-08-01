2491e5bcf990c40f7179372d3fc2dee17e4ce353
yangjing <yangjing@benpaobao.com>
2019-08-01 18:43:52 +0800

    update

---
+++ src/views/index.vue
@@ -0,0 +1,127 @@
+<style scoped>
+    .layout{
+        border: 1px solid #d7dde4;
+        background: #f5f7f9;
+        position: relative;
+        border-radius: 4px;
+        overflow: hidden;
+    }
+    .layout-header-bar{
+        background: #fff;
+        box-shadow: 0 1px 1px rgba(0,0,0,.1);
+    }
+</style>
+<template>
+    <div class="layout">
+        <Sider :style="{position: 'fixed', height: '100vh', left: 0, overflow: 'auto'}">
+            <Menu active-name="1-2" theme="dark" width="auto" :open-names="['1']">
+                <Submenu name="1">
+                    <template slot="title">
+                        <Icon type="ios-navigate"></Icon>
+                        Item 1
+                    </template>
+                    <MenuItem name="1-1">Option 1</MenuItem>
+                    <MenuItem name="1-2">Option 2</MenuItem>
+                    <MenuItem name="1-3">Option 3</MenuItem>
+                </Submenu>
+                <Submenu name="2">
+                    <template slot="title">
+                        <Icon type="ios-keypad"></Icon>
+                        Item 2
+                    </template>
+                    <MenuItem name="2-1">Option 1</MenuItem>
+                    <MenuItem name="2-2">Option 2</MenuItem>
+                </Submenu>
+                <Submenu name="3">
+                    <template slot="title">
+                        <Icon type="ios-analytics"></Icon>
+                        Item 3
+                    </template>
+                    <MenuItem name="3-1">Option 1</MenuItem>
+                    <MenuItem name="3-2">Option 2</MenuItem>
+                </Submenu>
+            </Menu>
+        </Sider>
+        <Layout :style="{marginLeft: '200px'}">
+            <Header :style="{background: '#fff', boxShadow: '0 2px 3px 2px rgba(0,0,0,.1)'}"></Header>
+            <Content :style="{padding: '0 16px 16px'}">
+                <Breadcrumb :style="{margin: '16px 0'}">
+                    <BreadcrumbItem>Home</BreadcrumbItem>
+                    <BreadcrumbItem>Components</BreadcrumbItem>
+                    <BreadcrumbItem>Layout</BreadcrumbItem>
+                </Breadcrumb>
+                <Card>
+                    <div style="height: 600px">
+                        <div style="margin-bottom:50px;">
+                            <template>
+                                <Input v-model="value" placeholder="Enter something..." style="width: 300px" />
+                            </template>
+                            <template>
+                                注册日期：<DatePicker type="daterange" split-panels placeholder="Select date" style="width: 200px"></DatePicker>
+                            </template>
+                        </div> 
+                        <div style="margin-bottom:50px;">
+                            <template>
+                                <Table :columns="columns1" :data="data1"></Table>
+                            </template>
+                        </div>
+                        <div style="text-align:center;">
+                            <template>
+                                <Page :total="100" />
+                            </template>
+                        </div>
+                    </div>
+                </Card>
+            </Content>
+        </Layout>
+    </div>
+</template>
+<script>
+    export default {
+        data() {
+            return {
+                value: '',
+                 columns1: [
+                    {
+                        title: 'Name',
+                        key: 'name'
+                    },
+                    {
+                        title: 'Age',
+                        key: 'age'
+                    },
+                    {
+                        title: 'Address',
+                        key: 'address'
+                    }
+                ],
+                data1: [
+                    {
+                        name: 'John Brown',
+                        age: 18,
+                        address: 'New York No. 1 Lake Park',
+                        date: '2016-10-03'
+                    },
+                    {
+                        name: 'Jim Green',
+                        age: 24,
+                        address: 'London No. 1 Lake Park',
+                        date: '2016-10-01'
+                    },
+                    {
+                        name: 'Joe Black',
+                        age: 30,
+                        address: 'Sydney No. 1 Lake Park',
+                        date: '2016-10-02'
+                    },
+                    {
+                        name: 'Jon Snow',
+                        age: 26,
+                        address: 'Ottawa No. 2 Lake Park',
+                        date: '2016-10-04'
+                    }
+                ]
+            }
+        }
+    }
+</script>
