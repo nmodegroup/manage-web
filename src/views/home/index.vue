4854af48c47c1a6f9f380af3ac238d75a308bc26
yangjing <369390743@qq.com>
2019-08-25 21:34:35 +0800

    update

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,6 +1,24 @@
 <template>
   <div>
-    
+    <div class="header-bar">顶部banner图管理</div>
+    <div class="index-body">
+      <div class="banner-item">
+        <Upload action="//jsonplaceholder.typicode.com/posts/">
+          <div class="banner-img">
+            <div class="banner-add">
+              <img src="../../../static/img/icon_add.png">
+              <div class="banner-add-tips">点击添加图片</div>
+            </div>
+          </div>
+        </Upload>
+        <div class="banner-btn-frame">
+          <div class="bnt-join">跳转关联</div>
+          <div class="toggle-frame">
+            
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
@@ -30,3 +48,27 @@ export default {
   }
 }
 </script>
+<style scoped>
+  .index-body {
+    display: flex;
+  }
+  .banner-item{
+    width:368px;
+    height:319px;
+    background:rgba(248, 248, 251, 1);
+    border-radius:10px;
+    margin-right: 30px;
+    padding:10px;
+  }
+  .banner-img {
+    height: 218px;
+    border-radius:4px;
+    border:1px dashed rgba(0,0,0,.2);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  .banner-add{
+    cursor: pointer;
+  } 
+</style>
