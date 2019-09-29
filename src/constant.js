d5e7aa117cc940cc3b23e2b9e503fe118bd68ace
yangjing <yangjing@benpaobao.com>
2019-09-29 21:23:03 +0800

    update

--- src/constant.js
+++ src/constant.js
@@ -1,7 +1,7 @@
 const constant = {};
 constant.globalData = {
-  baseURL: 'https://dev.api.nightmodeplus.com', //测试环境接口
-  //baseURL: 'https://www.nightmodeplus.com',  //生产环境的接口
+  // baseURL: 'https://dev.api.nightmodeplus.com', //测试环境接口
+  baseURL: 'https://www.api.nightmodeplus.com',  //生产环境的接口
 }
 
 export default constant;
