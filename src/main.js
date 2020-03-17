eaedba138e1c45ccd4d4e48502c54aa39fb9c840
kgm1818 <1634750252@qq.com>
2020-03-17 23:48:14 +0800

    url

--- src/main.js
+++ src/main.js
@@ -23,7 +23,8 @@ Vue.prototype.$goto = function (name, query, params) {
 }
 Vue.prototype.staticURL = function (path) {
   if (path) {
-    return  constant.globalData.baseURL + '/source/'+ path
+    // return  constant.globalData.baseURL + '/source/'+ path
+    return  constant.globalData.imgUrl+ path
   }
 }
 /* eslint-disable no-new */
